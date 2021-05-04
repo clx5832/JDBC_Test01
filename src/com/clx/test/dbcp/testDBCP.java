f312beab15bdcdfce1bef3661a6f4804e8663edb
CLX <3121378476@qq.com>
2021-05-04 15:56:49 +0800

    批量处理

---
+++ src/com/clx/test/dbcp/testDBCP.java
@@ -0,0 +1,18 @@
+package com.clx.test.dbcp;
+
+public class testDBCP {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 使用DBCP数据库连接池
+     * 1.加入jar包
+     * 2.创建数据库连接池
+     * */
+    public static void testDBCP(){
+
+        
+
+    }
+}
