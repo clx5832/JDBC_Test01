264c0aa5c588ee702c8af91429bcece3a1d6d62a
CLX <3121378476@qq.com>
2021-05-04 16:12:33 +0800

    批量处理

--- src/com/clx/test/dbcp/testDBCP.java
+++ src/com/clx/test/dbcp/testDBCP.java
@@ -1,4 +1,7 @@
 package com.clx.test.dbcp;
+import org.apache.commons.dbcp.BasicDataSource;
+
+import javax.sql.DataSource;
 
 public class testDBCP {
     public static void main(String[] args) {
@@ -11,8 +14,13 @@ public class testDBCP {
      * 2.创建数据库连接池
      * */
     public static void testDBCP(){
+        DataSource dataSource = null;
 
-        
+        //1.创建DBPC数据源实例
+        dataSource = new BasicDataSource();
+
+        //2.为数据源实例指定必须的属性
+
 
     }
 }
