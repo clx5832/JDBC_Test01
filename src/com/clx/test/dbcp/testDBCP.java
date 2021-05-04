8eef12e7e32bd3764830c224ff767bef8950bedf
CLX <3121378476@qq.com>
2021-05-04 16:30:27 +0800

    批量处理

--- src/com/clx/test/dbcp/testDBCP.java
+++ src/com/clx/test/dbcp/testDBCP.java
@@ -1,11 +1,14 @@
 package com.clx.test.dbcp;
-import org.apache.commons.dbcp.BasicDataSource;
 
-import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class testDBCP {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws SQLException {
+        testDBCP();
     }
 
     /**
@@ -13,13 +16,21 @@ public class testDBCP {
      * 1.加入jar包
      * 2.创建数据库连接池
      * */
-    public static void testDBCP(){
-        DataSource dataSource = null;
+    public static void testDBCP() throws SQLException {
+        BasicDataSource dataSource = null;
 
         //1.创建DBPC数据源实例
         dataSource = new BasicDataSource();
 
         //2.为数据源实例指定必须的属性
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/person");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 
+        //3.从数据源中获取数据库连接
+        Connection connection = dataSource.getConnection();
+
+        System.out.println(connection);
     }
 }
